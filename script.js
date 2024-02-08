44f2cb58f3ccf8519656014085c346ba1ec39af3
nazrellynnv <nazrinv07@gmail.com>
2024-02-08 14:51:47 +0400

    header is ready

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-menuBtn=document.querySelector('#menu-button');
+menuBtn=document.querySelector('.link-button');
+closeBtn=document.querySelector('#close-Menu')
 
 menuBtn.addEventListener("click", showMenu);
 
@@ -8,19 +9,54 @@ function showMenu(event) {
 
 
 }
+closeBtn.addEventListener("click", closeMenu);
+
+function closeMenu(event) {
+    event.preventDefault();
+    popupWindow.style.display = "none";
+
+
+}
 
 
 
-searchBtn=document.querySelector('#search-button');
+
 searchInput=document.querySelector('#search-input')
+searchBtn=document.querySelector('#search-button');
 
+searchBtn.addEventListener("click", ()=>{
+  searchBtn.style.transform = "rotate(720deg)"
+  searchBtn.style.opacity = "0"
+  searchInput.style.display = "block"
+})
 
-searchBtn.addEventListener("click", showInput);
-
+// searchBtn.addEventListener("click", showInput);
 
 function showInput() {
     searchInput.classList.toggle('active');
     searchBtn.classList.toggle('active');
     searchInput.focus()
+}
+
+
+
+
+card=document.querySelector("#card");
+cardBack=document.querySelector("#cardBack")
+
+card.addEventListener("mouseenter",  showBack);
+function showBack() {
+card.style.transform= "rotateY(180deg)";
+card.style.
+cardBack.style.display="block"
+
+
+
+}
+card.addEventListener("mouseleave", showFront);
+function showFront() {
+card.style.transform= "rotateY(0)";
+cardBack.style.display="none"
+
 
 }
